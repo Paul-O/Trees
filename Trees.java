b39aa67752587a43455e3a4fdf5571dea113c019
MSI\SoulExorcist <aragornbarbossa@gmail.com>
2017-04-30 18:22:39 -0400

    added spots for the methods

--- Trees.java
+++ Trees.java
@@ -1,4 +1,23 @@
 public class Trees
 {
-  
+  public static void main(String args[])
+  {
+    
+  }
+  public int search()
+  {
+    
+  }
+  public void inset()
+  {
+    
+  }
+  public void delete()
+  {
+    
+  }
+  public void print()
+  {
+    
+  }
 }
