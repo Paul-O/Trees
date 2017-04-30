b578700fe737a20128db01788a94617d0fe02604
MSI\SoulExorcist <aragornbarbossa@gmail.com>
2017-04-30 18:19:41 -0400

    finished node

--- Node.java
+++ Node.java
@@ -1,4 +1,25 @@
 public class Node
 {
-  
+  private int data;
+  private Node next;
+  public Node(int data)
+  {
+    this.data = data;
+  }
+  public void setData(int d)
+  {
+    this.data = d;
+  }
+  public int getData()
+  {
+    return data;
+  }
+  public void setNext(Node n)
+  {
+    this.next = n;
+  }
+  public Node getNext()
+  {
+    return next;
+  }
 }
