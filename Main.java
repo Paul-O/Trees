d6828fd04d5047ed629a38396457b5428ba4529d
MSI\SoulExorcist <aragornbarbossa@gmail.com>
2017-05-07 16:25:01 -0400

    created Main class

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.io*;
+public class Main
+{
+  public static void main(String args[]) throws IOException
+  {
+    File myFile = new File("lemons.txt");
+    Scanner scanner = new Scanner(myFile);
+  }
+}
