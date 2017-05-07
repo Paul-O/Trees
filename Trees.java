0190d210fefd353580bfe7dadfd86bdb07ffc67c
MSI\SoulExorcist <aragornbarbossa@gmail.com>
2017-05-07 17:20:39 -0400

    insert methods

--- Trees.java
+++ Trees.java
@@ -17,9 +17,69 @@ public class Trees
   {
     
   }
-  public void inset()
+  public void inset(String x)
   {
-    
+    if(isEmpty())
+    {
+      root = new Node(x);
+    }
+    else
+    {
+      Node temp = root;
+      while(temp != null)
+      {
+        if(temp.getData().equalsIgnoreCase(x) = 0)
+        {
+          temp.count ++;
+          break;
+        }
+        else if(temp.getData().equalsIgnoreCase(x) < 0)
+        {
+          temp.setRight(x);
+          temp.count ++;
+          break;
+        }
+        else if(temp.getData().equalsIgnoreCase(x) > 0)
+        {
+          temp.setLeft(x);
+          temp.count ++;
+          break;
+        }
+      }
+    }
+  }
+  public void insert(Node r, Node n)
+  {
+    if(n.getData().compareToIgnoreCase(r.getData()) < 0)
+    {
+      if(r.left != null)
+      {
+        insert(r.left, n);
+      }
+      else
+      {
+        r.left = n;
+        r.left.parent = r;
+        r.left ++;
+      }
+    }
+    else if(n.getData().compareToIgnoreCase(r.getData()) > 0)
+    {
+      if(r.right != null)
+      {
+        insert(r.right, n);
+      }
+      else
+      {
+        r.right = n;
+        r.right.parent = r;
+        r.right ++;
+      }
+    }
+    else if(n.getData().compareToIgnoreCase(r.getData()) = 0)
+    {
+      r.count ++;
+    }
   }
   public void delete()
   {
